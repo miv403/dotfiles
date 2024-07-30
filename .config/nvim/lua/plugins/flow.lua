31ef597ebbb6e80e2a3d03e6130834bc9eb7c7bf
miv403 <62180556+miv403@users.noreply.github.com>
2024-07-31 01:34:15 +0300

    added kitty and nvim config

---
+++ .config/nvim/lua/plugins/flow.lua
@@ -0,0 +1,17 @@
+return {
+  "0xstepit/flow.nvim",
+  lazy = false,
+  enabled = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+    require("flow").setup({
+      transparent = false, -- Set transparent background.
+      fluo_color = "green", --  Fluo color: pink, yellow, orange, or green.
+      mode = "bright", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
+      aggressive_spell = false, -- Display colors for spell check.
+    })
+
+    vim.cmd("colorscheme flow")
+  end,
+}
