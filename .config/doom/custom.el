0117a2ba1f1ed519e877d7f9e468fe3b1214900c
miv403 <62180556+miv403@users.noreply.github.com>
2024-08-10 19:51:59 +0300

    added doom emacs config

---
+++ .config/doom/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(delete-selection-mode nil)
+ '(org-agenda-files
+   '("/home/miv403/andaç/bilgisayar.org" "/home/miv403/andaç/kamp.org" "/home/miv403/andaç/programlama.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
