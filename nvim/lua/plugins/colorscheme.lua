b099e6d3790693aeb2625df7a3e883bd03f52dfb
miv403 <miv403@duck.com>
2026-03-20 19:16:57 +0300

    updated neovim config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
 
-  "LazyVim/LazyVim",
-  priority = 999, -- make sure to load this before all the other start plugins
-  opts = {
-    colorscheme = "retrobox",
-  },
+    "LazyVim/LazyVim",
+    priority = 99, -- make sure to load this before all the other start plugins
+    opts = {
+        colorscheme = "retrobox",
+    },
 }
